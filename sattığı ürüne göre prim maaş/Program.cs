1e6af0730438a23e524673a6ddcc6b9767b58105
FurkanPtn <furkanpatann@gmail.com>
2025-02-04 22:36:35 +0300

    Add project files.

---
+++ sattığı ürüne göre prim maaş/Program.cs
@@ -0,0 +1,29 @@
+﻿//  maaş = 200
+//  satış = brüt satışı giriniz
+//  sonmaaş = maaş + brüt*9/100
+//  print maaşınız = sonmaaş
+//  if ( satış == -1)
+//  break
+
+
+while (true)
+{
+    int salary = 200;
+    
+    Console.Write(" please enter your sale(satış) = ");
+   float sale = Convert.ToSingle(Console.ReadLine()); 
+
+    if (sale == -1)
+    {
+        break;
+    }
+
+    float brüt = salary + sale * 9 / 100;
+
+    Console.WriteLine($"your brüt is = {brüt}");
+}
+
+
+
+
+
